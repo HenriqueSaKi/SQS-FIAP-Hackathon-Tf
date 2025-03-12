1b5e4282307a6978aa296cf303c920521997e3a1
Henrique Kisaki <satoshikisaki@hotmail.com>
2025-03-11 23:38:10 -0300

    refactor: Update bucket S3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region         = "us-east-1"
-    bucket         = "sqs-fiap-hackathon-terraform-state-bucket"
+    bucket         = "fiap-hackathon-terraform-state-bucket"
     key            = "sqs/terraform.tfstate"
     encrypt        = true
   }
